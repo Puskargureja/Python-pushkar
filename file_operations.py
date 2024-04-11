2782e76e38ffb289a8584dca3e7f71518bbce7f8
waraicg <162769693+waraicg@users.noreply.github.com>
2024-04-11 14:58:26 -0400

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -4,8 +4,23 @@ File Operations Module
 This module provides functions for reading from and writing to the text file
 that stores employee data.
 """
-def read_employees():
-    """
+   def read_employees():
+    
+    with open('employees.txt', 'r') as file:
+
+        employees_data = file.readlines()
+
+
+    employees_data = [employee.strip() for employee in employees_data]
+
+ 
+    return employees_data
+       
+def write_employees(employees_data):
+ with open('employees.txt', 'w') as file:
+        for employee in employees_data:
+            file.write(f"{employee}\n") 
+            """
     Read Employees Function
 
     This function reads employee data from the text file and returns it.
@@ -14,13 +29,3 @@ def read_employees():
         list: A list containing employee data read from the text file.
     """
 
-
-def write_employees():
-    """
-    Write Employees Function
-
-    This function writes employee data to the text file.
-
-    Parameters:
-        employees_data (list): A list containing employee data to be written to the text file.
-    """
